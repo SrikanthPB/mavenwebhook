eee6dba46078aa2f234233a9e03fa7c6dbad7fe4
Srikanth <awstrialac@gmail.com>
2018-12-09 10:43:43 +0530

    commented

--- src/main/java/com/mkyong/controller/BaseController.java
+++ src/main/java/com/mkyong/controller/BaseController.java
@@ -6,7 +6,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-// written by srikanth
+// written by srikanth sim
 @Controller
 public class BaseController {
 
