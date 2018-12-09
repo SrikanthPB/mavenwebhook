f84ff5d2a1efa78534999d4fe7b4d35c756a4774
Srikanth <awstrialac@gmail.com>
2018-12-09 09:20:01 +0530

    commit all files

---
+++ src/main/java/com/mkyong/controller/BaseController.java
@@ -0,0 +1,39 @@
+package com.mkyong.controller;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class BaseController {
+
+	private static int counter = 0;
+	private static final String VIEW_INDEX = "index";
+	private final static org.slf4j.Logger logger = LoggerFactory.getLogger(BaseController.class);
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String welcome(ModelMap model) {
+
+		model.addAttribute("message", "Welcome");
+		model.addAttribute("counter", ++counter);
+		logger.debug("[welcome] counter : {}", counter);
+
+		// Spring uses InternalResourceViewResolver and return back index.jsp
+		return VIEW_INDEX;
+
+	}
+
+	@RequestMapping(value = "/{name}", method = RequestMethod.GET)
+	public String welcomeName(@PathVariable String name, ModelMap model) {
+
+		model.addAttribute("message", "Welcome " + name);
+		model.addAttribute("counter", ++counter);
+		logger.debug("[welcomeName] counter : {}", counter);
+		return VIEW_INDEX;
+
+	}
+
+}
